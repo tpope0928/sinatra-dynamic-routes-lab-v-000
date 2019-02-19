594ea59df5845aa40bbfac45d50985bddd1d99b7
Thomas Pope <tpope0928@gmail.com>
2019-02-19 01:32:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,7 +29,19 @@ class App < Sinatra::Base
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
 
+    answer = 'Unable to answer'
 
+    case params[:operation]
+
+    when 'add'
+      answer = (number1 + number2).to_s
+    when 'subtract'
+      answer = (number1 - number2).to_s
+    when 'multiply'
+      answer = (number1 * number2).to_s
+    when 'divide'
+      answer = (number1 / number2).to_s
+    end
   end
 
 end
