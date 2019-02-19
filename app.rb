af84558310df978fbef2d068d6b40f1707d03c9f
Thomas Pope <tpope0928@gmail.com>
2019-02-19 01:13:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,4 +21,8 @@ class App < Sinatra::Base
     response
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[word1]} #{params[word2]} #{params[word3]} #{params[word4]} #{params[word5]}"
+  end
+
 end
